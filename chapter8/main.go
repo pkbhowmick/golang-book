698e2eec0f754d6af012efc9e2cadbbc17fc5762
Pulak Kanti Bhowmick <pulak@appscode.com>
2020-12-17 18:05:40 +0600

    Add practice content of chapter8

---
+++ chapter8/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func swap(xPtr,yPtr *int)  {
+	temp := *xPtr
+	*xPtr = *yPtr
+	*yPtr = temp
+}
+
+func main()  {
+	x := 1
+	y := 2
+	swap(&x,&y)
+	fmt.Println(x,y)
+}
