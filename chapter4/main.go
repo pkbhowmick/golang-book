33a5726c275c57a524611c2ca6a98fae972f87f4
Pulak Kanti Bhowmick <pulak@appscode.com>
2020-12-17 11:41:15 +0600

    Add practice content of chapter4

---
+++ chapter4/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+const PI float64 = 3.1416
+var str string = "Global variable"
+
+func main()  {
+	// Variables
+	var x string
+	x = "Hello World"
+	fmt.Println(x)
+	y := "Hello"
+	fmt.Println(x==y)
+
+	// Understand variable scope
+	f()
+}
+
+func f()  {
+	fmt.Println(str)
+	fmt.Println(PI)
+	str = "another string"
+	fmt.Println(str)
+	// PI = 3.14 results in compile error
+}
