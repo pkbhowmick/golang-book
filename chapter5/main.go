f564d741af8f3f6e7462a23333ed3e85f0648650
Pulak Kanti Bhowmick <pulak@appscode.com>
2020-12-17 12:15:26 +0600

    Add practice content of chapter5

---
+++ chapter5/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// for loop and if-else
+	for i:=1; i<=10; i++ {
+		if i%2 == 0{
+			fmt.Println("Even")
+		} else{
+			fmt.Println("Odd")
+		}
+	}
+
+	// for loop with switch
+	fmt.Println("Now printing output for switchclear")
+	for i:=1; i<=10; i++ {
+		switch i%2 {
+		case 0: fmt.Println("Even")
+		case 1: fmt.Println("Odd")
+		}
+	}
+}
