0dd6340f0a1072ea106298a51a94216de1b266a0
Pulak Kanti Bhowmick <pulak@appscode.com>
2020-12-17 11:16:10 +0600

    Add practice content of chapter3

---
+++ chapter3/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// Types
+	fmt.Println("1 + 1 =", 1 + 1)
+	fmt.Println("1 + 1 =", 1.0 + 1.0)
+
+	// Strings
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello "+ "World")
+
+	// booleans
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
